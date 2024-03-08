ae53fcb4db4592f153819915431b8ee7b45baef1
Horizon <horizon52183@outlook.com>
2024-03-08 13:35:26 +0800

    homecontent test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import HomePage from "./pages/home";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Footer />
     </Router>
   );
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeArticle from '../components/homearticle'
+import HomeContent from '../components/homecontent'
+
+export default function Homepage() {
+    return (
+        <div>
+            <HomeContent />
+            <HomeArticle />
+        </div>
+    )
+}
