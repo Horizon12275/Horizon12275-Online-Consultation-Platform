2d91f8f35e48ae33c2cda80dde8a37a3a251bb46
Horizon <111064289+Horizon12275@users.noreply.github.com>
2024-05-14 08:34:04 +0800

    首页改好了

--- frontend/src/components/home_article.jsx
+++ frontend/src/components/home_article.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getRecommendedArticles } from "../services/articleService";
+import { ArticleList } from "./expert_articles";
 
 function ArticleCard({ article }) {
   return (
@@ -29,30 +30,24 @@ function ShowArticle() {
     });
   }, []);
   return (
-    <div className="gap-0 self-start mt-20 text-6xl tracking-tight leading-[64px] text-neutral-400 max-md:flex-wrap max-md:max-w-full max-md:text-4xl max-md:leading-[50px]">
-      <div className="flex flex-auto gap-3.5 max-md:flex-wrap">
-        <div className="flex flex-col grow shrink-0 gap-5 self-start mt-2 basis-0 w-fit">
-          <header className="gap-0 pr-24 text-6xl tracking-tight leading-[64px] text-neutral-400 max-md:text-4xl max-md:leading-[50px]">
-            <h1 className="text-5xl leading-[52px] text-black">
-              Read top articles from experts
-            </h1>
-            <p className="text-3xl leading-9 text-neutral-400 mt-4">
-              Articles keep you informed about various areas of expertise and
-              solve common problems.
-            </p>
-          </header>
-          <Link
-            to={`/article`}
-            className=" self-center mr-20 px-10 py-4 text-xl font-extrabold leading-5 text-center text-white bg-gray-900 rounded-full max-md:px-5"
-          >
-            See more articles
-          </Link>
-        </div>
-        {articles.map((article, index) => (
-          <ArticleCard key={index} article={article} />
-        ))}
+    <>
+      <div className="gap-0 self-start mt-20 text-6xl tracking-tight leading-[64px] text-neutral-400 max-md:flex-wrap max-md:max-w-full max-md:text-4xl max-md:leading-[50px]">
+        <h1 className="text-5xl leading-[52px] text-black">
+          Read top articles from experts
+        </h1>
+        <p className="text-3xl leading-9 text-neutral-400 mt-4">
+          Articles keep you informed about various areas of expertise and solve
+          common problems.
+        </p>
       </div>
-    </div>
+      <ArticleList articles={articles} />
+      <Link
+        to={`/article`}
+        className=" self-center mr-20 px-10 py-4 text-xl font-extrabold leading-5 text-center text-white bg-gray-900 rounded-full max-md:px-5"
+      >
+        See more articles
+      </Link>
+    </>
   );
 }
 
